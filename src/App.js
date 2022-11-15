1205e23b451f01d1e3914483da7f4ae40f3e1216
Aditya <adityabasak7031@gmail.com>
2022-11-15 19:35:17 +0530

    adding all files and check all dependencies last time before push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+import Show from './pages/Show';
+
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route exact path="/show/:id">
+          <Show />
+        </Route>
+
+        <Route>
+          <div>Not found</div>
+        </Route>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
